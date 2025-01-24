e7c005bbbc626c57a7f5625fbb1daedaf219c078
root <shossain@mit.edu>
2025-01-24 03:35:42 +0000

    wise dummy w directly using wise for model

--- runpod.sh
+++ runpod.sh
@@ -21,7 +21,7 @@ echo "Starting the first Python script (evaluate_util)..."
 master_port=18765
 split=forget10_perturbed
 model_family=llama2-7b
-experiment_name=WISE_incorrect
+experiment_name=WISE_dummy
 eval_name=${experiment_name}_${split}
 save_root=./model_outputs/${eval_name}
 model_path=/workspace/${experiment_name}/model
