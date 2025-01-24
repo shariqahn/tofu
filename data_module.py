2c2b4357a53dbbf3e35a41fa97dbd1dd5a4a6662
shariqahn <shossain@mit.edu>
2025-01-23 19:54:14 -0500

    restoer debug

--- data_module.py
+++ data_module.py
@@ -121,11 +121,7 @@ class TextDatasetQA(Dataset):
         # self.data = datasets.load_dataset(data_path, split)["train"].select(range(min(100, data_len)))
         # snh changed to load local; above already commented
         # self.data = datasets.load_dataset(data_path, split)["train"]
-        # self.data = datasets.load_from_disk(data_path)
-
-        data = datasets.load_from_disk(data_path)
-        length = len(data)
-        self.data = data.select(range(80, length))
+        self.data = datasets.load_from_disk(data_path)
 
         self.data = add_dataset_index(self.data)
         self.model_configs = get_model_identifiers_from_yaml(model_family)
